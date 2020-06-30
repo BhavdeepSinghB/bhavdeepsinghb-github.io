be21dd91cadc7be42d2201f29362d2c76fe369a1
Bhavdeep Singh <bhavdeepsmail@gmail.com>
2020-06-29 18:38:31 -0700

    Updates

---
+++ precache-manifest.aa5e799898afa493517474bc261eda97.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19fb5c210efb2d3e6793470039444279",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9aeb26501a866d8ce81e",
+    "url": "/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "1ee22bd1d87056e0d40d",
+    "url": "/static/css/main.8ace3dcf.chunk.css"
+  },
+  {
+    "revision": "9aeb26501a866d8ce81e",
+    "url": "/static/js/2.ce876e84.chunk.js"
+  },
+  {
+    "revision": "ab3ed49c9114327d36bee72995bc6076",
+    "url": "/static/js/2.ce876e84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ee22bd1d87056e0d40d",
+    "url": "/static/js/main.265e8ddf.chunk.js"
+  },
+  {
+    "revision": "8f3fa0c11d10220982ac",
+    "url": "/static/js/runtime-main.3ae9010a.js"
+  }
+]);
