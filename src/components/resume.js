7a505dc86dcd91543ef225d7615ff8921dfd29d9
Bhavdeep Singh <bhavdeepsmail@gmail.com>
2020-07-01 22:41:58 -0700

    Added resume, css file for resume

--- src/components/resume.js
+++ src/components/resume.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import "../Resume.css";
 
 class Resume extends Component {
     render() {
         return(
-            <div><h3>Resume Page</h3></div>
+            <div className="resume-container">
+                <div className="resume-main">
+                    <iframe id="resume-frame" title="resume" src="https://www.dropbox.com/s/h4wvdtg60rg35ts/BhavdeepSinghResume.pdf?raw=1"></iframe>
+                </div>
+            </div>
         )
     }
 }
